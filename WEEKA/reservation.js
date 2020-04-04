7be8c544839c71a70e92ec0f54a6446e15bb4d31
TheBrianSiu <57185326+TheBrianSiu@users.noreply.github.com>
2020-04-04 23:31:44 +0800

    Add files via upload

--- WEEKA/reservation.js
+++ WEEKA/reservation.js
@@ -1,4 +1,62 @@
 $(document).ready(function() {
-	var emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
-		
-}); // end ready
+
+
+   $("#arrival_date").focus();
+
+   var emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
+
+
+   $("#submit").click(function(){
+
+        if($("#arrival_date").val()=="")
+        {
+            $("#arrival_dateError").text("Enter arrival date");
+            return false;
+        }
+
+        else if($("#nights").val()=="")
+        {
+            $("#nightsError").text("Enter nights");
+            return false;
+        }
+
+        else if(isNaN($("#nights").val()))
+        {
+            $("#nightsError").text("The number of nights must be numeric.");
+            return false;
+        }
+
+        else if($("#name").val()=="")
+        {
+            $("#nameError").text("Enter name");
+            return false;
+        }
+
+        else if($("#email").val()=="")
+        {
+            $("#emailError").text("Enter email");
+            return false;
+        }
+
+        else if(!emailPattern.test($("#email").val()))
+
+        {
+            $("#emailError").text("must be the pattern that’s provided.");
+            return false;
+        }
+
+        else if($("#phone").val()=="")
+
+        {
+            $("#phoneError").text("Enter phone");
+            return false;
+        }
+
+        else{
+            return true;
+        }
+
+   });
+
+});
+
